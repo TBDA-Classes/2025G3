53b598e69e5d22675b57648b5d7c4cb8a9b20878
Kevesh1 <keivana@outlook.com>
2025-10-08 23:33:44 +0200

    typo

--- backend/database.py
+++ backend/database.py
@@ -10,12 +10,13 @@ Enter correct credentials below
 """
 
 load_dotenv()
+print(os.getenv("DB_PASSWORD"))
 
 def get_connection():
     conn = psycopg2.connect(
         host=os.getenv("DB_HOST"),      
-        port=os.get("DB_PORT"),
-        database=os.get("DB_NAME"),      
+        port=os.getenv("DB_PORT"),
+        database=os.getenv("DB_NAME"),      
         user=os.getenv("DB_USER"),          
         password=os.getenv("DB_PASSWORD"), 
         cursor_factory=RealDictCursor  
