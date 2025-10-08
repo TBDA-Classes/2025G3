94a7b67862654de1c1bfa356f8c63feebbc5529e
Kevesh1 <92094004+Kevesh1@users.noreply.github.com>
2025-10-08 23:37:30 +0200

    Update main.tsx

--- frontend/TBDA/src/main.tsx
+++ frontend/TBDA/src/main.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App"; // your current main.tsx can be renamed to App.tsx
+import App from "./App"; 
 
 const rootElement = document.getElementById("app");
 if (!rootElement) throw new Error("No root element found");
