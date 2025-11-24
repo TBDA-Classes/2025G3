c028d8ef65943a75b164ae315bde2c394aaac592
Laurene-Chesnel <laurene.chesnel@alumnos.upm.es>
2025-11-24 15:19:46 +0100

    Delete Extraction/Temperature data _ extraction.py

--- Extraction/Temperature data _ extraction.py
+++
@@ -1,7 +0,0 @@
-user = "lectura"
-password = "ncorrea#2022"
-host = "138.100.82.184"    
-port = "2345"               
-database = "2207"           
-
-engine = create_engine(f"postgresql+psycopg2://{user}:{password}@{host}:{port}/{database}")
